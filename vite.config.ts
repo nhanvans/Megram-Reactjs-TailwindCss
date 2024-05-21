08142320cffaccd580cf32d40cc2485f9626f4f7
nhanvans <95522601+nhanvans@users.noreply.github.com>
2024-05-22 00:19:00 +0700

    update init sign up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
+import path from "path"
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
+//   "@vitejs/plugin-react": "^4.2.1",
+// "@vitejs/plugin-react-swc": "^3.5.0",
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
