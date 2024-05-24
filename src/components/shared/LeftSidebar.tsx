98545486ae47ad7e46c3d2cdf975e6a18b83b86a
nhanvans <95522601+nhanvans@users.noreply.github.com>
2024-05-24 22:55:40 +0700

    update home page

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -47,7 +47,9 @@ const LeftSidebar = () => {
             return (
               <li
                 key={link.label}
-                className={`leftsidebar-link group ${isActive && "bg-primary-500"}`}
+                className={`leftsidebar-link group ${
+                  isActive && "bg-primary-500"
+                }`}
               >
                 <NavLink
                   to={link.route}
@@ -67,6 +69,15 @@ const LeftSidebar = () => {
           })}
         </ul>
       </div>
+
+      <Button
+        variant="ghost"
+        className="shad-button_ghost"
+        onClick={() => signOut()}
+      >
+        <img src="/assets/icons/logout.svg" alt="logout" />
+        <p className="small-medium lg:base-medium">Logout</p>
+      </Button>
     </nav>
   );
 };
