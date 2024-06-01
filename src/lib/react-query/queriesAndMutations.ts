e476c18f3a5cbc239d872a510d39a01b8ed1f426
hovannhan <hovannhan.dev@gmail.com>
2024-06-01 14:38:49 +0700

    Update queriesAndMutations.ts

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -175,16 +175,18 @@ export const useDeletePost = () => {
 export const useGetPosts = () => {
   return useInfiniteQuery({
     queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-    queryFn: getInfinitePosts,
-    // getNextPageParam: (lastPage) => {
-    //   if (lastPage && lastPage.documents.length === 0) {
-    //     return null;
-    //   }
+    queryFn: getInfinitePosts as any,
+    getNextPageParam: (lastPage: any) => {
+      console.log(lastPage);
+      
+      if (lastPage && lastPage?.documents.length === 0) {
+        return null;
+      }
 
-    //   const lastId = lastPage?.documents[lastPage?.documents.length - 1].$id;
+      const lastId = lastPage?.documents[lastPage?.documents.length - 1].$id;
 
-    //   return lastId;
-    // },
+      return lastId;
+    },
   });
 };
 
